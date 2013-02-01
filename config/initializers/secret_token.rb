37e967fdd937391c2f70a0aea3ecb259ab5225c0
Luís Mazoni <luis.mazoni@hp.com>
2013-02-01 17:26:41 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wong::Application.config.secret_token = '1e4371bdeaf2280eb723a4cc0a29ec98a175042e78a75ae9a6b1395f98fe0ad52156b0dde87cfb599a1d53062f40d23c26d5417f20f52f6bf307136c11802ca6'
